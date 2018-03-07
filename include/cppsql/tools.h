ea249971bd424000be97cf56e2a55a601b1cc00f
Mark Grimes <mark.grimes@rymapt.com>
2018-03-07 09:29:30 +0000

    Add some (brief) documentation to the mySQLToSQLiteBackup function

--- include/cppsql/tools.h
+++ include/cppsql/tools.h
@@ -4,6 +4,12 @@
 
 namespace cppsql
 {
+	/** @brief Copy the complete MySQL database to an SQLite file
+	 *
+	 * Will probably fail if the SQLite file already exists.
+	 * @author Mark Grimes (mark.grimes@rymapt.com)
+	 * @date 07/Mar/2018
+	 */
 	void mySQLToSQLiteBackup( const char* host, const char* user, const char* password, const char* database, const char* outputFile );
 } // end of namespace cppsql
 
