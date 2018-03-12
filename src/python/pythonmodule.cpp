47fb6a4a95441fc0300cf8fb3a3cd411f84613f0
Mark Grimes <mark.grimes@rymapt.com>
2018-03-12 16:36:49 +0000

    Add python interface

---
+++ src/python/pythonmodule.cpp
@@ -0,0 +1,30 @@
+#include <Python.h>
+#include "cppsql/tools.h"
+
+static PyObject* cppsql_mySQLToSQLiteBackup( PyObject *self, PyObject *args, PyObject *keywords )
+{
+	// Argument defaults
+	const char* host="localhost";
+	const char* user="root";
+	const char* password="root";
+	const char* database="ethoscope_db";
+	const char* outputFile="testdb.sqlite";
+	static const char* keywordList[]={"host","user","password","database","outputFile"};
+	// Parse the arguments
+	if( !PyArg_ParseTupleAndKeywords( args, keywords, "|sssss", const_cast<char**>(keywordList),
+		&host, &user, &password, &database, &outputFile ) ) return nullptr;
+
+	// Call the C++ function
+	cppsql::mySQLToSQLiteBackup( host, user, password, database, outputFile );
+	Py_RETURN_NONE;
+}
+
+static PyMethodDef SqlitedumpMethods[] = {
+	{ "mySQLBackup", (PyCFunction)cppsql_mySQLToSQLiteBackup, METH_VARARGS | METH_KEYWORDS, "Backup a MySQL database to a SQLite file" },
+	{ NULL, NULL, 0, NULL }
+};
+
+PyMODINIT_FUNC initcppsql(void)
+{
+	(void) Py_InitModule( "cppsql", SqlitedumpMethods );
+}
