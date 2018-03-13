a74396a8f3a49548ee482bbca24abfe628ef34b8
Mark Grimes <mark.grimes@rymapt.com>
2018-03-13 10:08:19 +0000

    Add python function to execute a statement on get the results in a callback

--- src/python/pythonmodule.cpp
+++ src/python/pythonmodule.cpp
@@ -1,5 +1,6 @@
 #include <Python.h>
 #include "cppsql/tools.h"
+#include "cppsql/IDatabase.h"
 
 static PyObject* cppsql_mySQLToSQLiteBackup( PyObject *self, PyObject *args, PyObject *keywords )
 {
@@ -19,8 +20,68 @@ static PyObject* cppsql_mySQLToSQLiteBackup( PyObject *self, PyObject *args, PyO
 	Py_RETURN_NONE;
 }
 
+static PyObject* cppsql_executeOnDatabase( PyObject *self, PyObject *args, PyObject *keywords )
+{
+	const char* location;
+	const char* sqlQuery;
+	PyObject* pCallback;
+	static const char* keywordList[]={"location","sqlQuery","callback",nullptr};
+	if( !PyArg_ParseTupleAndKeywords( args, keywords, "ssO", const_cast<char**>(keywordList),
+		&location, &sqlQuery, &pCallback ) ) return nullptr;
+	if( !PyCallable_Check(pCallback) )
+	{
+		PyErr_SetString(PyExc_TypeError, "callback parameter must be callable");
+		return nullptr;
+	}
+	// Not going to increment reference count on pCallback because I finish with
+	// it before returning from this function.
+	auto pDatabase=cppsql::openDatabase( location );
+	if( pDatabase==nullptr )
+	{
+		PyErr_SetString(PyExc_ValueError, "database location is not valid");
+		return nullptr;
+	}
+	PyObject* pArguments=PyTuple_New(3);
+	PyObject* pNumFields=nullptr;
+	PyObject* pReturnValue=nullptr;
+	const char* const* oldValues=nullptr;
+	const char* const* oldFieldNames=nullptr;
+	pDatabase->execute( sqlQuery, [pCallback,pArguments,&pNumFields,&oldValues,&oldFieldNames,&pReturnValue](int numFields,const char* const values[],const char* const fieldNames[]){
+		if( pNumFields==nullptr )
+		{
+			pNumFields = PyLong_FromLong(numFields);
+			PyTuple_SetItem(pArguments, 0, pNumFields);
+		}
+		if( oldValues!=values )
+		{
+			PyObject* pValue = PyLong_FromSize_t((size_t) values);
+			PyTuple_SetItem(pArguments, 1, pValue);
+			oldValues=values;
+		}
+		if( oldFieldNames!=fieldNames )
+		{
+			PyObject* pValue = PyLong_FromSize_t((size_t) fieldNames);
+			PyTuple_SetItem(pArguments, 2, pValue);
+			oldFieldNames=fieldNames;
+		}
+		pReturnValue = PyObject_CallObject(pCallback, pArguments);
+		if( pReturnValue==nullptr ) return false;
+
+		bool returnValue=PyBool_Check(pReturnValue);
+		Py_DECREF(pReturnValue);
+		return returnValue;
+	} );
+	Py_DECREF(pArguments);
+	// Pretty sure pNumFields is DECREF'd because it's owned by pArguments.
+	// pReturnValue will have been DECREF'd in the lambda.
+	if( pReturnValue==nullptr && pNumFields!=nullptr ) return nullptr; // if pNumFields is null no records were returned
+
+	Py_RETURN_NONE;
+}
+
 static PyMethodDef SqlitedumpMethods[] = {
 	{ "mySQLToSQLiteBackup", (PyCFunction)cppsql_mySQLToSQLiteBackup, METH_VARARGS | METH_KEYWORDS, "Backup a MySQL database to a SQLite file" },
+	{ "executeOnDatabase", (PyCFunction)cppsql_executeOnDatabase, METH_VARARGS | METH_KEYWORDS, "Execute an SQL query string on a database" },
 	{ NULL, NULL, 0, NULL }
 };
 
